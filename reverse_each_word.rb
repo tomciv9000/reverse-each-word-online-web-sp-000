db4ea9413cef0486c3ead2ede9e5d6b695d89ea1
Tom White <eugene.white@gmail.com>
2019-05-20 03:30:25 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+def reverse_each_word(string_sentence)
+  array_sentence = string_sentence.split(/ /)
+  reversed_array = []
+  array_sentence.each do |word|
+    reversed_array << word.reverse
+  end
+  reversed_array.join(" ")
+end
+
+def reverse_each_word(string_sentence)
+  array_sentence = string_sentence.split(/ /)
+  reversed_array = []
+  array_sentence.collect do |word|
+    reversed_array << word.reverse
+  end
+  reversed_array.join(" ")
+end
