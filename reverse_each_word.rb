8b0be13cd1dd7766af199b294b66d3ea7dce2245
Tom White <eugene.white@gmail.com>
2019-05-20 03:43:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,10 +5,6 @@ def reverse_each_word(string_sentence)
 end
 
 def reverse_each_word(string_sentence)
-  array_sentence = string_sentence.split(/ /)
-  reversed_array = []
-  array_sentence.collect do |word|
-    reversed_array << word.reverse
-  end
-  reversed_array.join(" ")
+  string_sentence.split(/ /).collect {|word| word.reverse} .join(" ")
 end
+
